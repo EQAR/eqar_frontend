dcb761d86bbb110e384d97e7b17ba7907b2a5a8d
Gyorgy Gabor <gyorgyg@gmail.com>
2018-02-19 23:15:23 +0100

    feat(equar frontend): set up coreui starter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {HashRouter, Route, Switch} from 'react-router-dom';
 
-class HelloWorld extends React.Component {
-    render() {
-        return <h1>Hello, world!</h1>;
-    }
-}
+// Styles
+// Import Font Awesome Icons Set
+import 'font-awesome/css/font-awesome.min.css';
+// Import Simple Line Icons Set
+import 'simple-line-icons/css/simple-line-icons.css';
+// Import Main styles for this application
+import '../scss/style.scss'
+// Temp fix for reactstrap
+import '../scss/core/_dropdown-menu-right.scss'
 
-const element = document.getElementById('hello-world');
+// Containers
+import Full from './containers/Full/'
 
-ReactDOM.render(<HelloWorld />, element);
+ReactDOM.render((
+  <HashRouter>
+    <Switch>
+      <Route path="/" name="Home" component={Full}/>
+    </Switch>
+  </HashRouter>
+), document.getElementById('root'));
