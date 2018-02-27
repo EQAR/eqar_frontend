71365a3e42211dc7045ccf0da8cf8086c378fd76
Gyorgy Gabor <gyorgyg@gmail.com>
2018-02-27 01:23:51 +0100

    feat(package.json): add redux

--- src/index.js
+++ src/index.js
@@ -1,27 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter, Route, Switch} from 'react-router-dom';
-
-// Styles
-// Import Font Awesome Icons Set
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import 'font-awesome/css/font-awesome.min.css';
-// Import Simple Line Icons Set
 import 'simple-line-icons/css/simple-line-icons.css';
-// Import Main styles for this application
-import '../scss/style.scss'
-// Temp fix for reactstrap
-import '../scss/core/_dropdown-menu-right.scss'
-
-// Containers
-import Full from './containers/Full/'
-
-import Login from './views/Login/'
+import '../scss/style.scss';
+import '../scss/core/_dropdown-menu-right.scss';
+import Full from './containers/Full/';
+import Login from './views/Login/';
 
 ReactDOM.render((
-  <HashRouter>
-    <Switch>
-      <Route exact path="/login" name="Login Page" component={Login}/>
-      <Route path="/" name="Home" component={Full}/>
-    </Switch>
-  </HashRouter>
+  <Provider store={store}>
+    <HashRouter>
+      <Switch>
+        <Route exact path="/login" name="Login Page" component={Login}/>
+        <Route path="/" name="Home" component={Full}/>
+      </Switch>
+    </HashRouter>
+  </Provider>
 ), document.getElementById('root'));
