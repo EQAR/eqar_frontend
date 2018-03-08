89df5c9b3aa901e6d02540ae0848643a6164d7c4
Gyorgy Gabor <gyorgyg@gmail.com>
2018-03-09 00:13:11 +0100

    feat(create report): create simple component

---
+++ src/views/CreateReport/CreateReport.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CreateReport extends Component {
+  render() {
+    return (
+      <div>
+        Hi
+      </div>
+    );
+  }
+}
+
+export default CreateReport;
