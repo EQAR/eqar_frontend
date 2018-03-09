ab16fa6982a4120738f0ed1a84afe434303260ee
Gyorgy Gabor <gyorgyg@gmail.com>
2018-03-09 02:15:32 +0100

    feat(report base data): create report base data form

--- src/views/CreateReport/CreateReport.js
+++ src/views/CreateReport/CreateReport.js
@@ -12,6 +12,7 @@ import {
   CardBody,
   CardHeader} from 'reactstrap';
 import classnames from 'classnames';
+import ReportBaseData from './components/ReportBaseData'
 
 
 class CreateReport extends Component {
@@ -70,7 +71,7 @@ class CreateReport extends Component {
                     </NavItem>
                     <NavItem>
                       <NavLink
-                        className={classnames({ active: this.state.activeTab === '3' })}
+                        className={classnames({ active: this.state.activeTab === '4' })}
                         onClick={() => { this.toggle('4'); }}
                       >
                         Report Files
@@ -83,11 +84,7 @@ class CreateReport extends Component {
             <CardBody>
               <TabContent activeTab={this.state.activeTab}>
                 <TabPane tabId="1">
-                  1. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
-                  et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                  aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-                  officia deserunt mollit anim id est laborum.
+                  <ReportBaseData />
                 </TabPane>
                 <TabPane tabId="2">
                   2. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
---
+++ src/views/CreateReport/components/ReportBaseData.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+import {
+  FormGroup,
+  Input,
+  Label,
+  Row,
+  Col,
+  Form} from 'reactstrap';
+
+class ReportBaseData extends Component {
+  render() {
+    return (
+      <div>
+        <Row>
+          <Col>
+            <FormGroup>
+              <Label for="localIdentifier">Password</Label>
+              <Input type="text" name="text" id="localIdentifier" placeholder="Enter the report local identifier" />
+            </FormGroup>
+          </Col>
+          <Col>
+            <FormGroup>
+              <Label for="agencyName">Agency</Label>
+              <Input type="select" name="select" id="agencyName">
+                <option>ACQUIN</option>
+                <option>MUSIQUE</option>
+              </Input>
+            </FormGroup>
+          </Col>
+          <Col>
+            <FormGroup>
+              <Label for="agencyActivity">Agency ESG Activity (Dependent of Agency)</Label>
+              <Input type="select" name="select" id="agencyActivity">
+                <option>Please Select</option>
+                <option>Programme accreditation in Germany</option>
+                <option>Instiutional accreditation in Austria</option>
+              </Input>
+            </FormGroup>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <FormGroup>
+              <Label for="reportTitle">Title of the report (Auto generated)</Label>
+              <Input type="text" name="text" id="reportTitle" placeholder="Enter the report local identifier" value="Programme external evaluation/accreditation (First Cycle, Second Cycle) (by ARACIS)" readOnly/>
+            </FormGroup>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <FormGroup>
+              <Label for="status">Status</Label>
+              <Input type="select" name="select" id="status">
+                <option>Please Select</option>
+                <option>part of obligatory EQA system</option>
+                <option>voluntary</option>
+              </Input>
+            </FormGroup>
+          </Col>
+          <Col>
+            <FormGroup>
+              <Label for="agencyActivity">Decision</Label>
+              <Input type="select" name="select" id="agencyActivity">
+                <option>Please Select</option>
+                <option>positive</option>
+                <option>positive with conditions or restrictions</option>
+                <option>negative</option>
+                <option>not applicable</option>
+              </Input>
+            </FormGroup>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <FormGroup>
+              <Label for="reportValidFrom">Report valid from</Label>
+              <Input type="date" name="date" id="reportValidFrom" placeholder="Enter the report local identifier" />
+            </FormGroup>
+          </Col>
+          <Col>
+            <FormGroup>
+              <Label for="reportValidTo">Report valid to</Label>
+              <Input type="date" name="date" id="reportValidTo" placeholder="Enter the report local identifier" />
+            </FormGroup>
+          </Col>
+        </Row>
+      </div>
+    )
+  }
+}
+
+export default ReportBaseData;
