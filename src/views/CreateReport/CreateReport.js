bde975171c05c0ee2ae2beb5206e6969e092adc2
Gyorgy Gabor <gyorgyg@gmail.com>
2018-03-09 01:11:23 +0100

    feat(create report): add tabs

--- src/views/CreateReport/CreateReport.js
+++ src/views/CreateReport/CreateReport.js
@@ -1,10 +1,112 @@
 import React, { Component } from 'react';
+import {
+  Badge,
+  Row,
+  Col,
+  TabContent,
+  TabPane,
+  Nav,
+  NavItem,
+  NavLink,
+  Card,
+  CardBody,
+  CardHeader} from 'reactstrap';
+import classnames from 'classnames';
+
 
 class CreateReport extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      activeTab: '1'
+    };
+  }
+
+  toggle(tab) {
+    if (this.state.activeTab !== tab) {
+      this.setState({
+        activeTab: tab
+      });
+    }
+  }
   render() {
     return (
-      <div>
-        Hi
+      <div className="animated fadeIn">
+        <Col>
+          <Card>
+            <CardHeader>
+              <Row>
+                <Col>
+                  <i className="icon-docs"></i>
+                  Create Report
+                </Col>
+                <Col xs="auto">
+                  <Nav tabs className="float-right">
+                    <NavItem>
+                      <NavLink
+                        className={classnames({ active: this.state.activeTab === '1' })}
+                        onClick={() => { this.toggle('1'); }}
+                      >
+                        Report Base Data
+                      </NavLink>
+                    </NavItem>
+                    <NavItem>
+                      <NavLink
+                        className={classnames({ active: this.state.activeTab === '2' })}
+                        onClick={() => { this.toggle('2'); }}
+                      >
+                        Institutions
+                      </NavLink>
+                    </NavItem>
+                    <NavItem>
+                      <NavLink
+                        className={classnames({ active: this.state.activeTab === '3' })}
+                        onClick={() => { this.toggle('3'); }}
+                      >
+                        Programmes
+                      </NavLink>
+                    </NavItem>
+                    <NavItem>
+                      <NavLink
+                        className={classnames({ active: this.state.activeTab === '3' })}
+                        onClick={() => { this.toggle('4'); }}
+                      >
+                        Report Files
+                      </NavLink>
+                    </NavItem>
+                  </Nav>
+                </Col>
+              </Row>
+            </CardHeader>
+            <CardBody>
+              <TabContent activeTab={this.state.activeTab}>
+                <TabPane tabId="1">
+                  1. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                  et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                  aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+                  officia deserunt mollit anim id est laborum.
+                </TabPane>
+                <TabPane tabId="2">
+                  2. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                  et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                  aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+                  officia deserunt mollit anim id est laborum.
+                </TabPane>
+                <TabPane tabId="3">
+                  2. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                  et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                  aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+                  officia deserunt mollit anim id est laborum.
+                </TabPane>
+              </TabContent>
+            </CardBody>
+          </Card>
+        </Col>
       </div>
     );
   }
