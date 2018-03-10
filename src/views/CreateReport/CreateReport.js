95aeb923d07cff3e55425a8a50d51ff533cc1854
Gyorgy Gabor <gyorgyg@gmail.com>
2018-03-10 13:30:51 +0100

    feat(create report form): add empty Institutions component

--- src/views/CreateReport/CreateReport.js
+++ src/views/CreateReport/CreateReport.js
@@ -12,7 +12,8 @@ import {
   CardBody,
   CardHeader} from 'reactstrap';
 import classnames from 'classnames';
-import ReportBaseData from './components/ReportBaseData'
+import ReportBaseData from './components/ReportBaseData';
+import Institutions from './components/Institutions'
 
 
 class CreateReport extends Component {
@@ -87,11 +88,7 @@ class CreateReport extends Component {
                   <ReportBaseData />
                 </TabPane>
                 <TabPane tabId="2">
-                  2. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
-                  et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                  aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-                  officia deserunt mollit anim id est laborum.
+                  <Institutions />
                 </TabPane>
                 <TabPane tabId="3">
                   2. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
---
+++ src/views/CreateReport/components/Institutions.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Institutions extends Component {
+  render() {
+    return (
+      <div>
+        Hi
+      </div>
+    )
+  }
+}
+
+export default Institutions;
