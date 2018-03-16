484b5b19fe9bbc767f4a94a31e42ae430a5b9b9a
Gyorgy Gabor <gyorgyg@gmail.com>
2018-03-16 14:00:12 +0100

    feat(selectInstitution): create proper country array

--- src/components/InstitutionsTable/Actions/selectInstitution.js
+++ src/components/InstitutionsTable/Actions/selectInstitution.js
@@ -2,6 +2,9 @@ import store from '../../../main_store';
 
 
 function selectInstitution(institution, institutions) {
+  institution.countries = [{
+    country: institution.countries
+  }]
   institutions.push(institution);
   store.dispatch({ type: 'SELECT_INSTITUTION', payload: institutions})
 }
