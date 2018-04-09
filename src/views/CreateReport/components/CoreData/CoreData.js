d10ce5ea49d737d9f7f77796961c4d4ebaae8eb0
Gyorgy Gabor <gyorgyg@gmail.com>
2018-04-10 00:49:05 +0200

    feat(Core Data): style info card

--- src/views/CreateReport/components/CoreData/CoreData.js
+++ src/views/CreateReport/components/CoreData/CoreData.js
@@ -6,6 +6,9 @@ import {
   Row,
   Card,
   CardBody,
+  CardHeader,
+  CardFooter,
+  Button,
   Col } from 'reactstrap';
 import { connect } from 'react-redux';
 import store from '../../../../main_store';
@@ -74,11 +77,36 @@ class CoreData extends Component {
                   <Label for="reportValidTo">Report valid to</Label>
                   <Input type="date" name="date" id="reportValidTo" placeholder="Enter the report local identifier" onChange={this.handleInput} />
                 </FormGroup>
+                <Card>
+                  <CardHeader>
+                    View Report on Agency Website
+                  </CardHeader>
+                  <CardBody>
+                    <FormGroup>
+                      <Label for="urlToReport">URL to Page</Label>
+                      <Input type="text" name="text" id="urlToReport" placeholder="Enter URL to page of report"/>
+                    </FormGroup>
+                    <FormGroup>
+                      <Label for="textOfUrl">Display Text for URL</Label>
+                      <Input type="text" name="text" id="textOfUrl" placeholder="Enter display text for URL"/>
+                    </FormGroup>
+                  </CardBody>
+                  <CardFooter>
+                    <Row>
+                      <Col>
+                        <Button color="primary">Add More</Button>
+                      </Col>
+                      <Col>
+                        <Button color="primary" className="float-right">Remove</Button>
+                      </Col>
+                    </Row>
+                  </CardFooter>
+                </Card>
               </CardBody>
             </Card>
           </Col>
           <Col xs="6">
-            <Card>
+            <Card className="info-box">
               <CardBody>
               </CardBody>
             </Card>
