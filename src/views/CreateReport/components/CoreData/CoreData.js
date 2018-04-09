c31789408f284bf69a0aba901a95ddf517a6e16f
Gyorgy Gabor <gyorgyg@gmail.com>
2018-04-10 00:14:24 +0200

    feat(eslint config) js can contain jsx

---
+++ src/views/CreateReport/components/CoreData/CoreData.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import {
+  FormGroup,
+  Input,
+  Label,
+  Row,
+  Card,
+  CardBody,
+  Col } from 'reactstrap';
+import { connect } from 'react-redux';
+import store from '../../../../main_store';
+import setStates from '../../../../state';
+import { formFill } from '../../Actions/reportFormActions';
+
+class CoreData extends Component {
+  constructor(props) {
+    super(props);
+    this.handleInput = this.handleInput.bind(this);
+  }
+
+  handleInput(e) {
+    formFill(e.target.value, e.target.id)
+  }
+
+  render() {
+    return (
+      <div>
+        <Row>
+          <Col xs="6">
+            <Card>
+              <CardBody>
+                <FormGroup>
+                  <Label for="agencyName">Agency</Label>
+                  <Input type="select" name="select" id="agencyName" onChange={this.handleInput}>
+                    <option>ACQUIN</option>
+                    <option>MUSIQUE</option>
+                  </Input>
+                </FormGroup>
+                <FormGroup>
+                  <Label for="agencyActivity">Agency ESG Activity</Label>
+                  <Input type="select" name="select" id="agencyActivity" onChange={this.handleInput}>
+                    <option>Please Select</option>
+                    <option>Programme accreditation in Germany</option>
+                    <option>Instiutional accreditation in Austria</option>
+                  </Input>
+                </FormGroup>
+                <FormGroup>
+                  <Label for="localIdentifier">Local Identifier</Label>
+                  <Input type="text" name="text" id="localIdentifier" placeholder="Enter the report local identifier" onChange={this.handleInput}/>
+                </FormGroup>
+                <FormGroup>
+                  <Label for="status">Status</Label>
+                  <Input type="select" name="select" id="status" onChange={this.handleInput}>
+                    <option>Please Select</option>
+                    <option>part of obligatory EQA system</option>
+                    <option>voluntary</option>
+                  </Input>
+                </FormGroup>
+                <FormGroup>
+                  <Label for="decision">Decision</Label>
+                  <Input type="select" name="select" id="decision" onChange={this.handleInput}>
+                    <option>Please Select</option>
+                    <option>positive</option>
+                    <option>positive with conditions or restrictions</option>
+                    <option>negative</option>
+                    <option>not applicable</option>
+                  </Input>
+                </FormGroup>
+                <FormGroup>
+                  <Label for="reportValidFrom">Report valid from</Label>
+                  <Input type="date" name="date" id="reportValidFrom" placeholder="Enter the report local identifier" onChange={this.handleInput} />
+                </FormGroup>
+                <FormGroup>
+                  <Label for="reportValidTo">Report valid to</Label>
+                  <Input type="date" name="date" id="reportValidTo" placeholder="Enter the report local identifier" onChange={this.handleInput} />
+                </FormGroup>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    )
+  }
+}
+
+export default connect(setStates)(CoreData);
