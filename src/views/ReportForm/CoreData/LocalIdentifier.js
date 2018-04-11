e05cf389a692613bd19ee8db36cc70e0ceb3e308
Gyorgy Gabor <gyorgyg@gmail.com>
2018-04-11 16:08:39 +0200

    feat(LocalIdentifier): create component

---
+++ src/views/ReportForm/CoreData/LocalIdentifier.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import {
+  FormGroup,
+  Input,
+  Label } from 'reactstrap';
+import { connect } from 'react-redux';
+import store from '../../../main_store';
+import setStates from '../../../state';
+import { formFill } from '../Actions/reportFormActions';
+
+
+class LocalIdentifier extends Component {
+  constructor(props) {
+    super(props);
+    this.handleInput = this.handleInput.bind(this);
+  }
+
+  handleInput(e) {
+    formFill(e.target.value, e.target.id);
+  }
+
+  render() {
+    return (
+      <FormGroup>
+        <Label for="localIdentifier">Local Report Identifier</Label>
+        <Input type="text" name="text" id="localIdentifier" placeholder="Enter the report local identifier" onChange={this.handleInput}/>
+      </FormGroup>
+    )
+  }
+}
+
+export default connect(setStates)(LocalIdentifier);
