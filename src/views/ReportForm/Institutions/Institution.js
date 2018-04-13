f2ba23af908de9111a25beaee22affc1b280ee22
Gyorgy Gabor <gyorgyg@gmail.com>
2018-04-13 11:45:01 +0200

    feat(institutions): autocomplete works

--- src/views/ReportForm/Institutions/Institution.js
+++ src/views/ReportForm/Institutions/Institution.js
@@ -11,51 +11,53 @@ import Select from 'react-select';
 import { connect } from 'react-redux';
 import store from '../../../main_store';
 import setStates from '../../../state';
+import Autosuggest from 'react-autosuggest';
 import { formFill } from '../Actions/reportFormActions';
-import { InstitutionSelect } from './actions';
+import { getInstitutions } from './actions';
 
 
 class Institution extends Component {
   constructor(props) {
     super(props);
-    this.handleInput = this.handleInput.bind(this);
-    this.getInstitutions = this.getInstitutions.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.getOptions = this.getOptions.bind(this);
     this.state = {
-      value: []
+      value: {}
     }
   }
 
-  handleInput(e) {
-    console.log(e.target)
-    InstitutionSelect(e.target.value);
+  handleChange(newValue) {
+    getInstitutions(newValue);
+    this.setState({
+      value: newValue
+    });
   }
 
-  getInstitutions() {
+  getOptions() {
     return this.props.institutions.institutions.map(institution => {
       return {
-        value: {
-          id: institution.id,
-          primaryName: institution.name_primary
-        },
+        value: institution,
         label: institution.name_primary
       }
     })
   }
 
   render() {
+    const options = this.getOptions();
+
     return (
       <FormGroup>
         <Label for="institution" className="required-input">Institution</Label>
-          <InputGroup>
-            <Select
-                  name="form-field-name2"
-                  value={this.state.value}
-                  options={this.getInstitutions()}
-                  onChange={this.handleInput}
-                />
-            <Input type="text" name="text" id="institution" placeholder="Enter institution information" onChange={this.handleInput}/>
-            <Button color="primary" onClick={this.handleClick}>Add</Button>
-          </InputGroup>
+          <Select
+            id="institution"
+            placeholder="Enter institution informations"
+            name="form-field-name"
+            value={this.state.value}
+            options={options}
+            onInputChange={this.handleChange}
+            onChange={this.handleChange}
+          />
+          <Button color="primary" onClick={this.handleClick}>Add</Button>
       </FormGroup>
     )
   }
--- src/views/ReportForm/Institutions/actions.js
+++ src/views/ReportForm/Institutions/actions.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import store from '../../../main_store';
 
 
-export function InstitutionSelect(filterValue) {
+export function getInstitutions(filterValue=null) {
   store.dispatch((dispatch) => {
     axios.get('https://backend.deqar.eu/adminapi/v1/select/institutions', {params: {query:filterValue}}).then((response) => {
       dispatch({ type: 'GET_INSTITUTIONS', payload: response.data});
