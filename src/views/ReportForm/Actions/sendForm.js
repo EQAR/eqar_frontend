40363ec17738b06ac78f07fba886baa252650d0b
Gyorgy Gabor <gyorgyg@gmail.com>
2018-04-17 17:54:05 +0200

    feat(send report): remove console.logs

--- src/views/ReportForm/Actions/sendForm.js
+++ src/views/ReportForm/Actions/sendForm.js
@@ -3,11 +3,9 @@ import store from '../../../main_store';
 
 
 function sendForm(formDatas) {
-  console.log(formDatas)
   formDatas.date_format = '%Y-%m-%d';
   formDatas.institutions = formDatas.institutions.map(institution => {
     return {deqar_id: institution.deqar_id}})
-  console.log(formDatas)
   axios.post('https://backend.deqar.eu/submissionapi/v1/submit/report', formDatas, {
         headers: {'Content-Type': 'application/json'}})
   .then((response) => {
