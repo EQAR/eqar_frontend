002708024375c31f482a2b5be9d21611eebc8d0d
Gyorgy Gabor <gyorgyg@gmail.com>
2018-04-18 16:11:17 +0200

    feat(core): add asigned activity

--- src/views/ReportForm/Actions/sendForm.js
+++ src/views/ReportForm/Actions/sendForm.js
@@ -3,17 +3,17 @@ import store from '../../../main_store';
 
 
 function sendForm(formDatas) {
-  formDatas.date_format = '%Y-%m-%d';
-  formDatas.institutions = formDatas.institutions.map(institution => {
-    return {deqar_id: institution.deqar_id}})
-  axios.post('https://backend.deqar.eu/submissionapi/v1/submit/report', formDatas, {
-        headers: {'Content-Type': 'application/json'}})
-  .then((response) => {
-    console.log(response.data)
-    dispatch(push('/'));
-  }).catch((err) => {
-    console.log(err.response.data)
-  });
+  // formDatas.date_format = '%Y-%m-%d';
+  // formDatas.institutions = formDatas.institutions.map(institution => {
+  //   return {deqar_id: institution.deqar_id}})
+  // axios.post('https://backend.deqar.eu/submissionapi/v1/submit/report', formDatas, {
+  //       headers: {'Content-Type': 'application/json'}})
+  // .then((response) => {
+  //   console.log(response.data)
+  //   dispatch(push('/'));
+  // }).catch((err) => {
+  //   console.log(err.response.data)
+  // });
 }
 
 export default sendForm;
