dd1fe3af8ceb2e2ea3b09280d55c478088ad4c9f
Gyorgy Gabor <gyorgyg@gmail.com>
2018-04-21 15:46:25 +0200

    feat(add programme): remove empty values from json

--- src/views/ReportForm/Programmes/AddProgramme.js
+++ src/views/ReportForm/Programmes/AddProgramme.js
@@ -5,16 +5,27 @@ import { connect } from 'react-redux';
 import store from '../../../main_store';
 import setStates from '../../../state';
 import { addProgrammeToReport, resetProgramme } from './actions';
+import lodash from 'lodash';
 
 
 class AddProgramme extends Component {
   constructor(props) {
     super(props);
     this.addProgramme = this.addProgramme.bind(this);
+    this.removeEmptyValues = this.removeEmptyValues.bind(this);
+  }
+
+  removeEmptyValues(obj, callback) {
+    lodash.forEach(obj, (val, k, obj) => {
+      if (val === '') {
+        lodash.unset(obj, k);
+      }
+    })
+    return obj;
   }
 
   addProgramme(event) {
-    addProgrammeToReport(this.props.programme, this.props.reportForm.programmes);
+    addProgrammeToReport(this.removeEmptyValues(this.props.programme, this.deleteObject), this.props.reportForm.programmes);
     resetProgramme();
   }
 
