3379b3be1854b4654b943a4d76591d8b0705edca
Gyorgy Gabor <gyorgyg@gmail.com>
2018-05-03 00:01:39 +0200

    feat(validation dates): more sofisticated masking and validation

--- src/views/ReportForm/CoreData/ValidFrom.js
+++ src/views/ReportForm/CoreData/ValidFrom.js
@@ -4,21 +4,42 @@ import {
   InputGroup,
   FormText,
   Label} from 'reactstrap';
-import { TextMask, InputAdapter } from 'react-text-mask-hoc';
+import MaskedInput from 'react-text-mask';
+import createAutoCorrectedDatePipe from 'text-mask-addons/dist/createAutoCorrectedDatePipe';
 import { connect } from 'react-redux';
 import store from '../../../main_store';
 import setStates from '../../../state';
 import { formFill } from '../Actions/reportFormActions';
+import FormAlert from '../FormAlert';
 
+
+const autoCorrectedDatePipe = createAutoCorrectedDatePipe('yyyy-mm-dd');
 
 class ValidFrom extends Component {
   constructor(props) {
     super(props);
     this.handleInput = this.handleInput.bind(this);
+    this.handleBlur = this.handleBlur.bind(this);
+    this.getErrorMessage = this.getErrorMessage.bind(this);
+    this.state = {
+      isAlert: false
+    }
+  }
+
+  handleBlur(e) {
+    if (e.target.value < this.props.agency.valid_from) {
+      formFill('', e.target.id);
+      this.setState({isAlert: true});
+    }
   }
 
   handleInput(e) {
-    formFill(e.target.value, e.target.id)
+    this.setState({isAlert: false});
+    formFill(e.target.value, e.target.id);
+  }
+
+  getErrorMessage() {
+    return 'The given date is earlier than the agency\'s validation date!'
   }
 
   render() {
@@ -26,17 +47,20 @@ class ValidFrom extends Component {
       <FormGroup>
         <Label for="reportValidFrom" className="required-input">Valid from</Label>
         <InputGroup>
-          <TextMask
+          <MaskedInput
             mask={[/\d/, /\d/, /\d/, /\d/, '-', /\d/, /\d/, '-', /\d/, /\d/]}
-            Component={InputAdapter}
             id="reportValidFrom"
             onChange={this.handleInput}
-            min={this.props.agency.valid_from}
-            value={this.props.reportForm.validFrom}
+            value={this.props.reportForm.valid_from}
             className="form-control"
+            pipe={autoCorrectedDatePipe}
+            keepCharPositions="true"
+            placeholder="yyyy-mm-dd"
+            onBlur={this.handleBlur}
           />
         </InputGroup>
         <FormText color="muted">ex. 2018-01-15</FormText>
+        <FormAlert isOpen={this.state.isAlert} alertMessage={this.getErrorMessage()}/>
       </FormGroup>
     )
   }
--- src/views/ReportForm/CoreData/ValidTo.js
+++ src/views/ReportForm/CoreData/ValidTo.js
@@ -4,39 +4,63 @@ import {
   Input, InputGroup,
   Label
 } from 'reactstrap';
-import { TextMask, InputAdapter } from 'react-text-mask-hoc';
+// import { TextMask, InputAdapter } from 'react-text-mask-hoc';
 import { connect } from 'react-redux';
 import store from '../../../main_store';
 import setStates from '../../../state';
 import { formFill } from '../Actions/reportFormActions';
+import MaskedInput from 'react-text-mask';
+import createAutoCorrectedDatePipe from 'text-mask-addons/dist/createAutoCorrectedDatePipe';
+import FormAlert from '../FormAlert';
 
+
+const autoCorrectedDatePipe = createAutoCorrectedDatePipe('yyyy-mm-dd');
 
 class ValidTo extends Component {
   constructor(props) {
     super(props);
     this.handleInput = this.handleInput.bind(this);
+    this.handleBlur = this.handleBlur.bind(this);
+    this.getErrorMessage = this.getErrorMessage.bind(this);
+    this.state = {
+      isAlert: false
+    }
+  }
+  handleBlur(e) {
+    if (e.target.value > this.props.agency.valid_to) {
+      formFill('', e.target.id);
+      this.setState({ isAlert: true });
+    }
   }
 
   handleInput(e) {
-    formFill(e.target.value, e.target.id)
+    this.setState({ isAlert: false });
+    formFill(e.target.value, e.target.id);
+  }
+
+  getErrorMessage() {
+    return 'The given date is later than the agency\'s registration end!'
   }
 
   render() {
     return (
       <FormGroup>
-        <Label for="reportValidTo">Valid from</Label>
+        <Label for="reportValidTo" className="required-input">Valid to</Label>
         <InputGroup>
-          <TextMask
+          <MaskedInput
             mask={[/\d/, /\d/, /\d/, /\d/, '-', /\d/, /\d/, '-', /\d/, /\d/]}
-            Component={InputAdapter}
             id="reportValidTo"
             onChange={this.handleInput}
-            min={this.props.agency.valid_to}
-            value={this.props.reportForm.validTo}
+            value={this.props.reportForm.valid_to}
             className="form-control"
+            pipe={autoCorrectedDatePipe}
+            keepCharPositions="true"
+            placeholder="yyyy-mm-dd"
+            onBlur={this.handleBlur}
           />
         </InputGroup>
         <FormText color="muted">ex. 2018-01-25</FormText>
+        <FormAlert isOpen={this.state.isAlert} alertMessage={this.getErrorMessage()}/>
       </FormGroup>
     )
   }
