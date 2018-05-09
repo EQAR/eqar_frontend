6a86f809ad1a5e54a96ec12dc483da18c2601fca
JoshBone <jozsef.bone@gmail.com>
2018-05-09 22:07:01 +0200

    Implement API Token change.

--- src/views/MyProfile/ChangeTokenForm.js
+++ src/views/MyProfile/ChangeTokenForm.js
@@ -6,79 +6,90 @@ import {
   CardBody,
   CardHeader,
   CardFooter,
-  Button
+  FormGroup, Label
 } from 'reactstrap';
-import { Formik, Form } from 'formik';
-import Yup from 'yup'
-import axios from "axios/index";
+import { Form, Text } from 'react-form';
+import LaddaButton, {EXPAND_RIGHT} from "react-ladda";
 import { toast } from 'react-toastify';
-import TextInput from "../../components/FormComponents/TextInput";
-import { POST_PASSWORD } from '../../config';
+import 'ladda/dist/ladda-themeless.min.css';
+import axios from "axios/index";
+import { POST_TOKEN } from '../../config';
+import changeToken from "./Actions/ChangeToken";
 
-const validationSchema = Yup.object().shape({
-    token: Yup.string(),
-});
 
-const initialValues = {
-  token: localStorage.getItem('token'),
-};
+class ChangeTokenForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+    }
+  }
 
-const onSubmit = (values, { resetForm, setErrors, setStatus, setSubmitting}) => {
-  return axios.post(POST_PASSWORD, values)
-    .then(function (response) {
-      toast.success("Password has been updated!");
-      resetForm();
-    })
-    .catch(function (error) {
-      if (error.response.status === 400) {
-        setErrors(error.response.data);
-      }
-      toast.error("Error!");
+  getCurrentToken() {
+    return {api_token: localStorage.getItem('token')};
+  }
+
+  loadingToggle() {
+    this.setState({
+      ...this.state,
+      loading: !this.state.loading
     });
-};
+  }
 
-const ChangeTokenForm = () => (
-  <Card>
-    <CardHeader>
-      <Row>
-        <Col>API Token</Col>
-      </Row>
-    </CardHeader>
-    <Formik
-      initialValues={initialValues}
-      onSubmit={onSubmit}
-      validationSchema={validationSchema}
-      render={({
-        values,
-        errors,
-        touched,
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        isSubmitting,
-      }) => (
-        <div>
-          <Form>
-            <CardBody>
-              <TextInput
-                name="token"
-                type="input"
-                label="API Token"
-                error={touched.token && errors.token}
-                value={values.token}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                readOnly={true}
-              />
-            </CardBody>
-            <CardFooter>
-              <Button type="submit" size="sm" disabled={true} color="primary">Generate New</Button>
-            </CardFooter>
-          </Form>
-        </div>
-      )}
-      />
-  </Card>
-);
+  checkSubmitState(formState) {
+    if(formState.errors || Object.keys(formState.touched).length === 0) {
+      return true;
+    }
+  }
+
+  handleSubmit(value, e, formApi) {
+    this.loadingToggle();
+    axios.post(POST_TOKEN, value)
+      .then( response => {
+        this.loadingToggle();
+        toast.success("Token has been updated!");
+        formApi.setValue('api_token', response.data.token);
+        localStorage.setItem('token', response.data.token);
+        axios.defaults.headers.common['authorization'] = `Bearer ${response.data.token}`;
+      })
+      .catch( error => {
+        this.loadingToggle();
+        toast.error("There was a problem updating your API token.");
+      });
+  }
+
+  render() {
+    return (
+      <Card>
+        <CardHeader>
+          <Row>
+            <Col>Change API Token</Col>
+          </Row>
+        </CardHeader>
+        <Form onSubmit={this.handleSubmit.bind(this)} defaultValues={this.getCurrentToken()}>
+          {formApi => (
+            <form onSubmit={formApi.submitForm} id="changeTokenForm">
+              <CardBody>
+                <FormGroup>
+                  <Label htmlFor="api_token">API Token</Label>
+                  <Text field="api_token" id="apiToken" className={'form-control'} readOnly={true}/>
+                </FormGroup>
+              </CardBody>
+              <CardFooter>
+                <LaddaButton
+                  className="btn btn-primary btn-ladda btn-sm"
+                  loading={this.state.loading}
+                  data-color="blue"
+                  data-style={EXPAND_RIGHT}>
+                  Submit
+                </LaddaButton>
+              </CardFooter>
+            </form>
+          )}
+        </Form>
+      </Card>
+    );
+  }
+}
 
 export default ChangeTokenForm;
