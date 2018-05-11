56f15e8f4f9986edf321768d015d260da208cf55
Gyorgy Gabor <gyorgyg@gmail.com>
2018-05-11 13:16:03 +0200

    feat(InstitutionForm): insert customInput

--- src/views/services/InstitutionForm/index.js
+++ src/views/services/InstitutionForm/index.js
@@ -12,16 +12,22 @@ import { connect } from 'react-redux';
 import store from '../../../main_store';
 import setStates from '../../../state';
 import { closeInstitutionForm } from './actions';
+import { CustomInputField } from './CustomInputs';
 
 
 class InstitutionModal extends Component {
   constructor(props) {
     super(props);
     this.toggle = this.toggle.bind(this);
+    this.handleInput = this.handleInput.bind(this);
   }
 
   toggle() {
     closeInstitutionForm();
+  }
+
+  handleInput(e) {
+    console.log(e.target.value, e.target.id);
   }
 
   render() {
@@ -34,7 +40,60 @@ class InstitutionModal extends Component {
             <Col xs="6">
               <Card className="info-box">
                 <CardBody>
+                  <CustomInputField
+                    labelText="Institution Name, Official"
+                    labelClassName="required-input"
+                    type="text"
+                    id="name_official"
+                    name="text"
+                    handleInput={this.handleInput}
+                    />
+                  <CustomInputField
+                    labelText="Institution Name, Transliterated"
+                    type="text"
+                    id="name_official_transliterated"
+                    name="text"
+                    handleInput={this.handleInput}
+                    />
+                  <CustomInputField
+                    labelText="Institution Name, English"
+                    type="text"
+                    id="name_english"
+                    name="text"
+                    handleInput={this.handleInput}
+                    />
+                  <CustomInputField
+                    labelText="Institution Acronym"
+                    type="text"
+                    id="acronym"
+                    name="text"
+                    handleInput={this.handleInput}
+                    />
 
+
+                  <CustomInputField
+                    labelText="National Identifier"
+                    type="text"
+                    id="national_identifier"
+                    name="text"
+                    handleInput={this.handleInput}
+                    />
+                  <CustomInputField
+                    labelText="Local Identifier"
+                    type="text"
+                    id="local_identifier"
+                    name="text"
+                    handleInput={this.handleInput}
+                    />
+
+
+                  <CustomInputField
+                    labelText="Institution Website"
+                    type="text"
+                    id="website_link"
+                    name="text"
+                    handleInput={this.handleInput}
+                    />
                 </CardBody>
 
               </Card>
